396bd923b5c4a5caf75e3df0b70c0fc550fb0684
Nowshin Amin <nowshinaminbd@gmail.com>
2026-07-27 22:51:11 +0600

    update database connection

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -11,11 +11,12 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.log("Database Connection Failed");
-    console.log(err);
-  } else {
-    console.log("MySQL Connected Successfully");
+    console.log("❌ Database Connection Failed");
+    console.log(err.message);
+    return;
   }
+
+  console.log("✅ MySQL Connected Successfully");
 });
 
 module.exports = db;
