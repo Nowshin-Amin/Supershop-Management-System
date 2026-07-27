cb09856443a87da7a60afafb6eeedf323fec9f4f
Nowshin Amin <nowshinaminbd@gmail.com>
2026-07-27 23:28:26 +0600

    fix category api export and deployment urls

--- client/src/api/categoryApi.js
+++ client/src/api/categoryApi.js
@@ -1,16 +1,29 @@
 import axios from "axios";
 
+export const API =
+  "https://supershop-backend-1sfq.onrender.com/api/categories";
 
-export const API = "https://supershop-backend-1sfq.onrender.com/api/categories";
 
+// Get all categories
+export const getCategories = () =>
+  axios.get(API);
+
+
+// Get single category
 export const getCategory = (id) =>
   axios.get(`${API}/${id}`);
 
+
+// Add category
 export const addCategory = (category) =>
   axios.post(API, category);
 
+
+// Update category
 export const updateCategory = (id, category) =>
   axios.put(`${API}/${id}`, category);
 
+
+// Delete category
 export const deleteCategory = (id) =>
   axios.delete(`${API}/${id}`);
